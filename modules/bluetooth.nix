96693ce479d70f058c9de793033ef48b556e379f
areas0 <areas0@outlook.com>
2024-11-25 22:18:22 +0100

    feat: added bluetooth

---
+++ modules/bluetooth.nix
@@ -0,0 +1,10 @@
+{ ... }:
+# Enable bluetooth
+{
+  hardware.bluetooth = {
+    enable = true;
+    settings = { General = { Enable = "Source,Sink,Media,Socket"; }; };
+  };
+
+  services = { blueman.enable = true; };
+}
