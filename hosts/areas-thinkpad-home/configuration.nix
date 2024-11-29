d9ba1693426e51675515cff887acfff659715211
areas0 <areas0@outlook.com>
2024-11-29 21:55:29 +0100

    feat: added grub configuration custom

--- hosts/areas-thinkpad-home/configuration.nix
+++ hosts/areas-thinkpad-home/configuration.nix
@@ -6,12 +6,19 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.efiSupport = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    useOSProber = true;
+    efiSupport = true;
+    configurationLimit = 1;
+
+    splashImage = ../../assets/xenoblade.jpg;
+    default = "saved";
+  };
+
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.configurationLimit = 1;
+
 
   networking.hostName = "areas-thinkpad"; # Define your hostname.
 
