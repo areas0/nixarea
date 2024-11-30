70c3884d08f5f3d46d469b470991394474f600e1
areas0 <areas0@outlook.com>
2024-11-30 13:31:37 +0100

    feat: added thunar and removed duplicate hyprlock

--- home/home.nix
+++ home/home.nix
@@ -123,71 +123,11 @@
     pkgs.jellyfin-media-player
 
     pkgs.zen-browser
+
+    # thunar file manager
+    pkgs.xfce.thunar
+    pkgs.xfce.thunar-archive-plugin
   ];
-
-  programs.hyprlock = {
-    enable = true;
-
-    settings = {
-      general = {
-        disable_loading_bar = true;
-        hide_cursor = false;
-        no_fade_in = true;
-      };
-
-      background = [
-        {
-          monitor = "";
-          color = "rgba(17, 17, 17, 1.0)";
-        }
-      ];
-
-      input-field = [
-        {
-          # monitor = "eDP-1";
-
-          size = "300, 50";
-
-          outline_thickness = 1;
-
-          outer_color = "rgb(b6c4ff)";
-          inner_color = "rgb(dce1ff)";
-          font_color = "rgb(354479)";
-
-          fade_on_empty = false;
-          placeholder_text = ''<span foreground="##354479">Password...</span>'';
-
-          dots_spacing = 0.2;
-          dots_center = true;
-        }
-      ];
-
-      label = [
-        {
-          monitor = "";
-          text = "$TIME";
-          font_size = 50;
-          color = "rgb(b6c4ff)";
-
-          position = "0, 150";
-
-          valign = "center";
-          halign = "center";
-        }
-        {
-          monitor = "";
-          text = "cmd[update:3600000] date +'%a %b %d'";
-          font_size = 20;
-          color = "rgb(b6c4ff)";
-
-          position = "0, 50";
-
-          valign = "center";
-          halign = "center";
-        }
-      ];
-    };
-  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
