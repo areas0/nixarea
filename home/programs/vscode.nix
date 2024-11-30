74d8e79f7d5862727faacdf9db783a1bc9e3304f
areas0 <areas0@outlook.com>
2024-11-30 11:51:26 +0100

    feat: switch to vscode unstable

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
   programs.vscode = {
     enable = true;
+    package = pkgs-unstable.vscode;
 
-    extensions = with pkgs.vscode-extensions; [
+    extensions = with pkgs-unstable.vscode-extensions; [
       github.copilot
       pkief.material-icon-theme
       ms-azuretools.vscode-docker
